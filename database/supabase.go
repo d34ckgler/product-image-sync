ccfde6d906bdef9f89d42bf3901b02eda7bfa094
d34ckgler <herogamers.ortiz@gmail.com>
2025-03-28 02:22:06 -0400

    [REFACTOR]: refactor_supabase_and_functions_edge_api

---
+++ database/supabase.go
@@ -0,0 +1,137 @@
+package database
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/d34ckgler/sync-product-image/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Supabase struct {
+	Host     string
+	User     string
+	Password string
+	Port     string
+	Dbname   string
+	DB       *gorm.DB
+	sql      *sql.DB
+}
+
+type Ecommerce interface {
+	Open()
+	InitPool() (*gorm.DB, error)
+	Connect()
+	GetProducts() ([]model.Product, error)
+	GetProduct(id int) (model.Product, error)
+	GetProductBySku(sku string) (model.Product, error)
+	Create(value interface{}) (tx *gorm.DB)
+	Save(value interface{}) (tx *gorm.DB)
+	Close() error
+}
+
+func (ecm *Supabase) InitPool() (*gorm.DB, error) {
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Caracas application_name=ecommerce", ecm.Host, ecm.User, ecm.Password, ecm.Dbname, ecm.Port)
+	// dsn := "postgresql://postgres.gpacszhbwbrdjyyxlzdr:6Dv0YvCiGBJmNYT3@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
+
+	// dsn := "postgresql://postgres.gpacszhbwbrdjyyxlzdr:6Dv0YvCiGBJmNYT3@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
+	dsn := "postgresql://postgres:postgres@127.0.0.1:54322/postgres"
+
+	// sqlDB, err := sql.Open("pgx", dsn)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to create connection pool: %v", err)
+	// }
+
+	// sqlDB.SetMaxIdleConns(10)
+	// sqlDB.SetMaxOpenConns(100)
+	// sqlDB.SetConnMaxLifetime(30)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: nil,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	ecm.sql, err = db.DB()
+	if err != nil {
+		return nil, err
+	}
+	ecm.DB = db
+
+	return db, nil
+}
+
+func (ecm *Supabase) Connect() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Caracas application_name=ecommerce", ecm.Host, ecm.User, ecm.Password, ecm.Dbname, ecm.Port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+		Logger:      nil,
+	})
+	if err != nil {
+		panic("failed to connect database server")
+	}
+
+	ecm.sql, err = db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	ecm.DB = db
+}
+
+func (ecm *Supabase) GetProducts() (products []model.Product, err error) {
+	ecm.DB.Find(&products)
+
+	if len(products) <= 0 {
+		return nil, errors.New("not found products")
+	}
+
+	return
+}
+
+func (ecm *Supabase) Desallocate() {
+	_, err := ecm.sql.Exec("DEALLOCATE ALL;")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (ecm *Supabase) GetProduct(id int) (product model.Product) {
+	ecm.DB.First(&product, id)
+
+	return
+}
+
+func (ecm *Supabase) GetMedia(slug string) (media model.Media) {
+	ecm.DB.First(&media, "slug = ?", slug)
+
+	return
+}
+
+func (ecm *Supabase) GetProductBySku(sku string) (product model.Product) {
+	ecm.DB.Where("sku = ?", sku).Find(&product)
+
+	return
+}
+
+func (ecm *Supabase) Create(value interface{}) (tx *gorm.DB) {
+	// ecm.Desallocate()
+	return ecm.DB.Create(value)
+}
+
+func (ecm *Supabase) Save(value interface{}) (tx *gorm.DB) {
+	// ecm.Desallocate()
+	return ecm.DB.Save(value)
+}
+
+func (ecm *Supabase) Close() error {
+	sql, err := ecm.DB.DB()
+	if err != nil {
+		return err
+	}
+
+	return sql.Close()
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"image/jpeg"
@@ -12,18 +11,16 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/chai2010/webp"
+	"github.com/d34ckgler/sync-product-image/database"
+	"github.com/d34ckgler/sync-product-image/model"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mdb *mongo.Database
-var token string
-var PERCENT_DISCOUNT_STOCK float64
-var MONGODB_URI, MONGODB_DATABASE, API_BASE_URL, PREFIX_AUTHORIZATION, API_ADMIN_USER, API_ADMIN_PASSWORD, ENDPOINT_USER_LOGIN, ENDPOINT_MEDIA, WATCH_FILE_PATH, OUTPUT_FILE_PATH string
+var supabase *database.Supabase
+var MONGODB_URI, MONGODB_DATABASE, TOKEN, API_BASE_URL, PREFIX_AUTHORIZATION, API_ADMIN_USER, API_ADMIN_PASSWORD, ENDPOINT_USER_LOGIN, ENDPOINT_MEDIA, WATCH_FILE_PATH, OUTPUT_FILE_PATH string
 var err error
 
 func init() {
@@ -43,23 +40,22 @@ func init() {
 	MONGODB_DATABASE = os.Getenv("MONGODB_DATABASE")
 	WATCH_FILE_PATH = os.Getenv("WATCH_FILE_PATH")
 	OUTPUT_FILE_PATH = os.Getenv("OUTPUT_FILE_PATH")
+	TOKEN = os.Getenv("TOKEN")
+
+	// Supabase String Connection
+	SUPABASE_HOST := os.Getenv("sp_host")
+	SUPABASE_USER := os.Getenv("sp_user")
+	SUPABASE_PWD := os.Getenv("sp_password")
+	SUPABASE_PORT := os.Getenv("sp_port")
+	SUPABASE_DB := os.Getenv("sp_dbname")
+
+	supabase = &database.Supabase{Host: SUPABASE_HOST, User: SUPABASE_USER, Password: SUPABASE_PWD, Port: SUPABASE_PORT, Dbname: SUPABASE_DB}
+	// supabase.Connect()
+	_, err = supabase.InitPool()
+	if err != nil {
+		log.Fatal(err)
+	}
 	// end initialize environment variables
-}
-
-func CreateMongoConnection() *mongo.Database {
-	clientOptions := options.Client().ApplyURI(MONGODB_URI)
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB")
-	return client.Database(MONGODB_DATABASE)
 }
 
 func fetch(method, path, payload string) map[string]interface{} {
@@ -70,8 +66,8 @@ func fetch(method, path, payload string) map[string]interface{} {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("User-Agent", "ms-stellar-depoprod")
 	///////////////// add token to header /////////////////
-	authorization := fmt.Sprintf("%s %s", PREFIX_AUTHORIZATION, token)
-	req.Header.Add("Authorization", authorization)
+	// authorization := fmt.Sprintf("%s %s", PREFIX_AUTHORIZATION, token)
+	// req.Header.Add("Authorization", authorization)
 
 	res, _ := http.DefaultClient.Do(req)
 
@@ -86,14 +82,6 @@ func fetch(method, path, payload string) map[string]interface{} {
 	var jsonResponse map[string]interface{}
 	json.Unmarshal(body, &jsonResponse)
 	return jsonResponse
-}
-
-func getToken() string {
-	// payload := "{\"email\":\"dortiz@balystiendas.com\",\"password\":\"yitp.HBBM0KD\"}"
-	payload := fmt.Sprintf("{\"email\":\"%s\",\"password\":\"%s\"}", API_ADMIN_USER, API_ADMIN_PASSWORD)
-	jsonResponse := fetch("POST", ENDPOINT_USER_LOGIN, payload)
-
-	return jsonResponse["token"].(string)
 }
 
 func convertToWebp(sku string) {
@@ -129,15 +117,15 @@ func convertToWebp(sku string) {
 	}
 }
 
-func preparePayload(sku string) (*bytes.Buffer, string) {
+func preparePayload(product model.Product) (*bytes.Buffer, string) {
 	// create buffer
 	var b bytes.Buffer
 	writer := multipart.NewWriter(&b)
 
 	// Add the file to hold the form
-	file, err := os.Open(OUTPUT_FILE_PATH + "/" + sku + ".webp")
+	file, err := os.Open(OUTPUT_FILE_PATH + "/" + product.Sku + ".webp")
 	if err != nil {
-		fmt.Printf("[%s]: media not exist\n", sku)
+		fmt.Printf("[%s]: media not exist\n", product.Sku)
 		return nil, ""
 	}
 	defer file.Close()
@@ -155,7 +143,27 @@ func preparePayload(sku string) (*bytes.Buffer, string) {
 	}
 
 	// Add the other fields
-	err = writer.WriteField("alt", "product image")
+	err = writer.WriteField("alt", product.Description)
+	if err != nil {
+		fmt.Println("Error writing field: ", err)
+		return nil, ""
+	}
+
+	err = writer.WriteField("slug", product.Sku)
+	if err != nil {
+		fmt.Println("Error writing field: ", err)
+		return nil, ""
+	}
+
+	err = writer.WriteField("table_name", "product")
+	if err != nil {
+		fmt.Println("Error writing field: ", err)
+		return nil, ""
+	}
+
+	productID := strconv.Itoa(int(product.ProductID))
+
+	err = writer.WriteField("column_id", productID)
 	if err != nil {
 		fmt.Println("Error writing field: ", err)
 		return nil, ""
@@ -170,15 +178,15 @@ func preparePayload(sku string) (*bytes.Buffer, string) {
 	return &b, writer.FormDataContentType()
 }
 
-func uploadImage(image *bytes.Buffer, wHeader string) map[string]interface{} {
+func uploadImage(image *bytes.Buffer, wHeader string) interface{} {
 	req, err := http.NewRequest(http.MethodPost, API_BASE_URL+ENDPOINT_MEDIA, image)
 	if err != nil {
 		fmt.Println("Error creating request: ", err)
 	}
 
-	req.Header.Add("User-Agent", "ms-stellar-depoprod")
+	req.Header.Add("User-Agent", "ms-product-image")
 	req.Header.Add("Content-Type", wHeader)
-	req.Header.Add("Authorization", fmt.Sprintf("%s %s", PREFIX_AUTHORIZATION, token))
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", PREFIX_AUTHORIZATION, TOKEN))
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -194,51 +202,46 @@ func uploadImage(image *bytes.Buffer, wHeader string) map[string]interface{} {
 		fmt.Println("Error reading response: ", err)
 		return nil
 	}
-	var data map[string]interface{}
+	var data interface{}
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		fmt.Println("Error unmarshalling response: ", err)
 		return nil
 	}
 
-	fmt.Println(data["message"])
-	return data["doc"].(map[string]interface{})
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		fmt.Println("Error uploading image: ", data.(map[string]interface{})["message"])
+		return nil
+	}
+
+	fmt.Println(data)
+	return data.(map[string]interface{})["data"]
 }
 
 func main() {
-	mdb = CreateMongoConnection()
-	collectionProduct := mdb.Collection("productos")
-	token = getToken()
+	defer supabase.Close()
 
 	// Read All Products
-	cursor, err := collectionProduct.Find(context.TODO(), bson.M{})
+	// supabase
+	products, err := supabase.GetProducts()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
-	for cursor.Next(context.TODO()) {
-		var product map[string]interface{}
-		err = cursor.Decode(&product)
+	for _, product := range products {
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		// check if image exist
-		collectionMedia := mdb.Collection("media")
-		var data = bson.M{}
-		result := collectionMedia.FindOne(context.TODO(), bson.D{
-			{
-				Key:   "filename",
-				Value: fmt.Sprintf("%s.webp", product["sku"]),
-			}})
+		// logic
+		media := supabase.GetMedia(product.Sku)
 
-		err := result.Decode(&data)
-		if err != nil && err.Error() == "mongo: no documents in result" {
+		if media.MediaID == 0 {
 			// convert to webp
-			convertToWebp(product["sku"].(string))
+			convertToWebp(product.Sku)
 
 			// buffer
-			payload, wHeader := preparePayload(product["sku"].(string))
+			payload, wHeader := preparePayload(product)
 			if payload == nil {
 				continue
 			}
@@ -247,24 +250,11 @@ func main() {
 			if image == nil {
 				continue
 			}
-			fmt.Println(product["sku"], image["id"])
+			fmt.Println(product.Sku, image.([]interface{})[0].(map[string]interface{})["image_id"].(uint))
 
-			// update product
-			update := bson.M{
-				"$set": bson.M{
-					"images": []bson.D{
-						{
-							{"image", image["id"]},
-							{"id", image["id"]},
-						}},
-				},
-			}
-			_, err = collectionProduct.UpdateOne(context.TODO(), bson.M{"sku": product["sku"]}, update)
-			if err != nil {
-				fmt.Println(err)
-			}
+			// sintax update product here
 		} else {
-			fmt.Println(product["sku"], "exist")
+			fmt.Println(product.Sku, "exist")
 			continue
 		}
 
